0d5b5047d6313a1171e3d6a83042c27c12885491
Andrei Luiz Fernando <82054188+andreiluizf@users.noreply.github.com>
2022-01-26 11:32:56 -0300

    Update learn.js

--- learn.js
+++ learn.js
@@ -25,3 +25,87 @@ function depthProblem() {
       total -= 2;
   }
 } 
+
+
+function convertRate() {
+  var amount = parseFloat(readLine(), 10);
+  var rate = parseFloat(readLine(), 10);
+  
+  console.log(convert(amount, rate));
+
+  function convert() {
+    amount *= rate;
+    return amount;
+  }
+}
+
+function incPrices() {
+  var increase = parseInt(readLine(), 10);
+  var prices = [98.99, 15.2, 20, 1026];
+  
+  for (let i = 0; i < prices.length; i++) {
+      prices[i] += increase
+  }
+
+  console.log(prices)    
+}
+
+function computedProperties() {
+  var i = 0
+  var a = {
+    ['foo_' + ++i]: i,
+    ['foo_' + ++i]: i,
+    ['foo_' + ++i]: i
+  }
+  console.log(a)
+
+  function computedProperties2() {
+
+    let prop = 'name'
+    let id = '1234'
+    let mobile = '08923'
+  
+    let user = {
+      [prop]: 'Jack',
+      [`user_${id}`]: `${mobile}`
+    }
+  
+    console.log(user) // output: { name: 'Jack', user_1234: '08923' }
+  } // computedProperties2()
+
+  function computedProperties3() {
+
+    var param = 'size'
+    var config = {
+      [param]: 12,
+      ['mobile' + param.charAt(0).toUpperCase() + param.slice(1)]: 4
+    }
+    console.log(config.mobileSize)
+  } // computedProperties3()
+
+} // computedProperties()
+
+function objAssign() {
+
+  let person = {
+    name: 'Jack',
+    age: 18,
+    sex: 'male'
+  }
+  
+  let student = {
+    name: 'Bob',
+    age: 20,
+    xp: '2'
+  }
+  let newStudent = Object.assign({}, person, student) // { name: 'Bob', age: 20, sex: 'male', xp: '2' }
+  // console.log(newStudent)
+} // objAssign()
+
+function objDestructuring() {
+
+  let a,b
+  ({a,b} = {a: 'Hello', b: 'Jack'})
+
+  console.log(`${a} ${b}!`)
+} // objDestructuring()
