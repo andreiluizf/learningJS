ae895138122341049926aabd59edd573cd8aac7f
Andrei Luiz Fernando <82054188+andreiluizf@users.noreply.github.com>
2022-01-27 11:19:56 -0300

    Update learn.js

--- learn.js
+++ learn.js
@@ -109,3 +109,27 @@ function objDestructuring() {
 
   console.log(`${a} ${b}!`)
 } // objDestructuring()
+
+function spreadOperators() {
+
+  function containsAll(myArray, ...nums) {
+    // for (let k = 1; k < arguments.length; k++) {
+    for (let num of nums) {
+      // let num = arguments[k]
+      if (myArray.indexOf(num) === -1) {
+        return false
+      }
+    }
+    return true
+  }
+    // let x = [2,4,6,7]
+    // console.log(containsAll(x,2,4,7))
+    // console.log(containsAll(x,6,4,9))
+
+  function magic(...nums) {
+    let sum = 0;
+    nums.filter(n => n % 2 == 0).map(el => sum += el)
+    return sum
+  }
+  //console.log(magic(1,2,3,4,5,6))
+}
