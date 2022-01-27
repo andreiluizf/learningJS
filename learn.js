a761f840fc607eac322d13519408f12c97da1dde
Andrei Luiz Fernando <82054188+andreiluizf@users.noreply.github.com>
2022-01-27 11:47:41 -0300

    Update learn.js

--- learn.js
+++ learn.js
@@ -1,53 +1,55 @@
-function soma() {
-  var sum=0; 
-  for(i=4; i<8; i++) {
-    if (i == 6) {
-      continue; 
-    }
-    sum += i;
-  }
-  document.write(sum);
-} // soma()
-
-function depthProblem() {
-  var depth = parseInt(readLine(), 10);
-  //your code goes here
-  
-  var day = 0;
-  var total = 0;
-  while(total<depth){
-      day += 1;
-      total += 7;
-      if(total >= depth){
-          console.log(day);
-          break;
+function simpleFunctions() {
+  function soma() {
+    var sum=0; 
+    for(i=4; i<8; i++) {
+      if (i == 6) {
+        continue; 
       }
-      total -= 2;
+      sum += i;
+    }
+    document.write(sum);
+  } // soma()
+
+  function depthProblem() {
+    var depth = parseInt(readLine(), 10);
+    //your code goes here
+
+    var day = 0;
+    var total = 0;
+    while(total<depth){
+        day += 1;
+        total += 7;
+        if(total >= depth){
+            console.log(day);
+            break;
+        }
+        total -= 2;
+    }
+  } 
+
+
+  function convertRate() {
+    var amount = parseFloat(readLine(), 10);
+    var rate = parseFloat(readLine(), 10);
+
+    console.log(convert(amount, rate));
+
+    function convert() {
+      amount *= rate;
+      return amount;
+    }
   }
-} 
 
+  function incPrices() {
+    var increase = parseInt(readLine(), 10);
+    var prices = [98.99, 15.2, 20, 1026];
 
-function convertRate() {
-  var amount = parseFloat(readLine(), 10);
-  var rate = parseFloat(readLine(), 10);
-  
-  console.log(convert(amount, rate));
+    for (let i = 0; i < prices.length; i++) {
+        prices[i] += increase
+    }
 
-  function convert() {
-    amount *= rate;
-    return amount;
+    console.log(prices)    
   }
-}
-
-function incPrices() {
-  var increase = parseInt(readLine(), 10);
-  var prices = [98.99, 15.2, 20, 1026];
-  
-  for (let i = 0; i < prices.length; i++) {
-      prices[i] += increase
-  }
-
-  console.log(prices)    
 }
 
 function computedProperties() {
